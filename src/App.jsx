c1519de4e0a726ba42c5cd9b825806aee05481a6
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-19 19:57:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {shortList, list, longList} from "./data"
+import Carousel from "./Carousel";
+
+const App = () => {
+  return (
+    <main>
+      <Carousel/>
+     
+    </main>
+  );
+};
+export default App;
